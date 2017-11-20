ab7bbe51237ef5c85f676ca50179e6dfac4f0eff
carpenter007 <elias@zimi.de>
2017-11-20 20:34:15 +0100

    Add weight regularizer (L2) to model

--- model.py
+++ model.py
@@ -53,14 +53,15 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 
 model = Sequential()
 model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(24,5,5,subsample=(2,2),W_regularizer = l2(0.01),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),W_regularizer = l2(0.01),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),W_regularizer = l2(0.01),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
